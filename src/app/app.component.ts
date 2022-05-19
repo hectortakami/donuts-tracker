0ba14ddabde4c8ee2a61ae9b247fb72b7cbfa92b
Hector M. Takami <hector.m.takami@gmail.com>
2022-05-18 20:42:33 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+import { CreatorsComponent } from './dialogs/creators/creators.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'donuts-tracker';
+  constructor(private dialogService: NbDialogService) {}
+
+  date = new Date();
+  users: { name: string; title: string }[] = [
+    { name: 'Carla Espinosa', title: 'Este Googler debe donas! 🍩' },
+    { name: 'Bob Kelso', title: 'Este Googler debe tacos! 🌮' },
+    { name: 'Janitor', title: 'Este Googler debe carnita asada! 🥩' },
+    { name: 'Carla Espinosa', title: 'Este Googler debe donas! 🍩' },
+    { name: 'Bob Kelso', title: 'Este Googler debe tacos! 🌮' },
+    { name: 'Janitor', title: 'Este Googler debe carnita asada! 🥩' },
+    { name: 'Carla Espinosa', title: 'Este Googler debe donas! 🍩' },
+    { name: 'Bob Kelso', title: 'Este Googler debe tacos! 🌮' },
+    { name: 'Janitor', title: 'Este Googler debe carnita asada! 🥩' },
+    { name: 'Carla Espinosa', title: 'Este Googler debe donas! 🍩' },
+    { name: 'Bob Kelso', title: 'Este Googler debe tacos! 🌮' },
+    { name: 'Janitor', title: 'Este Googler debe carnita asada! 🥩' },
+  ];
+
+  open() {
+    this.dialogService.open(CreatorsComponent, { autoFocus: true });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbTabsetModule,
+  NbCardModule,
+  NbListModule,
+  NbUserModule,
+  NbButtonModule,
+  NbIconModule,
+  NbDialogModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { CreatorsComponent } from './dialogs/creators/creators.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CreatorsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbEvaIconsModule,
+    NbTabsetModule,
+    NbCardModule,
+    NbListModule,
+    NbUserModule,
+    NbButtonModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    NbDialogModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
